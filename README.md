7348cfa4baad79e6a943b41b9faf758f4260f2ed
Matt Adorjan <matt.adorjan@gmail.com>
2017-10-17 19:29:53 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# AWS re:Invent 2017 Schedule Extract Tool
+
+This tool is meant to make it super easy to export your re:invent schedule into a text file and then import it into whatever tool makes it easier for you to work with.
+
+This tool is given without warranty and probably little maintenance. I whipped it up over the weekend to try to solve my problems (explained below) and thought I would share.
+
+For some reason, when looking at the total # of sessions on the re:Invent Session Catalog, that number is higher than if you look at the totals for each day in the catalog. I have not spent much time investigating why there is this discrepancy.
+
+I didn't see anything in the re:Invent TOS regarding scraping schedule content. If I missed something, I'm happy to remove the tool.
+
+## TL;DR How to use the tool:
+1. In the reinvent.py file, update your event website credentials in the USERNAME and PASSWORD vars. These are the credentials you use when logging in on this page: https://www.portal.reinvent.awsevents.com/connect/login.ww. 
+2. Download the Chrome web driver for your OS (https://sites.google.com/a/chromium.org/chromedriver/downloads).
+3. Change the CHROME_DRIVER var to point to the driver location.
+4. Run the file in Python. Assuming all goes well, you should end up with a sessions.txt, pipe delimited text file with all of the re:Invent sessions, and a column indicating whether you marked it as a session you'd be interesting in attending.
+
+## Why did I make this?
+A couple of reasons:
+1. I can always do better with critical thinking and planning when I have something in front of me in a spreadsheet or table that I can manipulate. Yes, the re:Invent website gives an ok experience for browsing, but when given over 1000 sessions and then trying to figure out 1st, 2nd, and 3rd choice, it just is not intuitive for that purpose. Having all of this data available in Excel makes the planning process much easier.
+2. The "Get More Results" functionality doesn't work sometimes. I'm not sure if this is just me, or if others experience it, but there will be times when I'm looking at all of the sessions and trying to keep scrolling down, and the "Get More Results" link will not work. I've only been through a few pages of results, when I know there are hundreds more sessions. This got very frustrating.
+
+## Why did I use a combination of Selenium and BeautifulSoup?
+One reason: the "Get More Results" button is a JavaScript call, and I could not get that to work in BS for obvious reasons (not an actual browser). I played around with trying to determine if there was an API available for getting actual event information (similar to the event time information I found), but I was not able to find something.
---
+++ reinvent.py
@@ -0,0 +1,163 @@
+############################################################################################
+#### AWS re:Invent 2017 - Session Information Downloader
+# Provides a quick dirty way to export AWS re:Invent session content from the event website.
+# Requirements:
+#   1. Update your event website credentials in the USERNAME and PASSWORD vars.
+#   2. Download the Chrome web driver (https://sites.google.com/a/chromium.org/chromedriver/downloads).
+#   3. Change the CHROME_DRIVER var to point to the driver location.
+#
+# @author Matt Adorjan
+# @email matt.adorjan@gmail.com
+############################################################################################
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+import os
+import requests
+from time import sleep
+from bs4 import BeautifulSoup
+import re
+
+# On the re:Invent event site, each day has a numeric code assigned to it
+# This is Sunday, Monday, Tuesday, Wednesday, Thursday, Friday
+DAY_CODES = [130, 170, 31, 110, 111, 112]
+
+# Set username and password for reinvent event website
+USERNAME = 'USERNAME'
+PASSWORD = 'PASSWORD'
+
+# Chrome web driver path
+CHROME_DRIVER = './chromedriver'
+
+# Initialize headless chrome
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+content_to_parse = ''
+
+driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=CHROME_DRIVER)
+
+def login(chrome_driver, username, password):
+    '''
+    Handle user login to the reinvent session catalog.
+    Utilizes headless chrome, passing in username and password
+    '''
+    chrome_driver.get("https://www.portal.reinvent.awsevents.com/connect/login.ww")
+    username_field = chrome_driver.find_element_by_id("loginUsername")
+    username_field.send_keys(username)
+    password_field = chrome_driver.find_element_by_id("loginPassword")
+    password_field.send_keys(password)
+    login_button = chrome_driver.find_element_by_id("loginButton")
+    login_button.click()
+
+def get_session_time(session_id):
+    '''
+    Calls the API on the reinvent event website which returns session times.
+    Outputs a JSON object with time and room information for a specific session.
+    '''
+    url = 'https://www.portal.reinvent.awsevents.com/connect/dwr/call/plaincall/ConnectAjax.getSchedulingJSON.dwr'
+    data = {
+        "callCount": 1,
+        "windowName": "",
+        "c0-scriptName": "ConnectAjax",
+        "c0-methodName": "getSchedulingJSON",
+        "c0-id": 0,
+        "c0-param0": "number:" + session_id,
+        "batchId": 5,
+        "instanceId": 0,
+        "page": "%2Fconnect%2Fsearch.ww",
+        "scriptSessionId": "1234567"
+    }
+    headers = {'Content-Type': 'text/plain'}
+    r = requests.post(url, headers=headers, data=data)
+    returned = r.content
+    returned = returned.replace("\\", '')
+
+    # Returns in XHR format. Strip out the relevant information.
+    start_time = re.search(r"startTime\":(\".*?\")", returned, re.DOTALL | re.MULTILINE).group(1)
+    end_time = re.search(r"endTime\":(\".*?\")", returned, re.DOTALL | re.MULTILINE).group(1)
+    room = re.search(r"room\":(\".*?\")", returned, re.DOTALL | re.MULTILINE).group(1)
+
+    time_information = {
+        "start_time": start_time.replace('"', ''),
+        "end_time": end_time.replace('"', ''),
+        "room": room.replace('"', '')
+    }
+
+    return time_information
+
+# Login to the reinvent website
+login(driver, USERNAME, PASSWORD)
+
+# Getting content by day, instead of the entire set, because sometimes the
+# Get More Results link stops working on the full list. Haven't had issues
+# looking at the lists day by day.
+for day in DAY_CODES:
+    driver.get("https://www.portal.reinvent.awsevents.com/connect/search.ww#loadSearch-searchPhrase=&searchType=session&tc=0&sortBy=daytime&dayID="+str(day))
+    sleep(3)
+    print ("Getting Content for Day Code: " + str(day))
+    more_results = True
+    # Click through all of the session results pages for a specific day.
+    # The goal is to get the full list for a day loaded.
+    while(more_results):
+        try:
+            # Find the Get More Results link and click it to load next sessions
+            get_results_btn = driver.find_element_by_link_text("Get More Results")
+            get_results_btn.click()
+            sleep(3)
+        except NoSuchElementException as e:
+            more_results = False
+
+    # Once all sessions for the day have been loaded by the headless browser,
+    # append to a variable for use in BS.
+    content_to_parse = content_to_parse + driver.page_source.encode('utf-8')
+
+driver.close()
+
+# Start the process of grabbing out relevant session information and writing to a file
+soup = BeautifulSoup(content_to_parse, "html5lib")
+
+# In some event titles, there are audio options available inside of an 'i' tag
+# Strip out all 'i' tags to make this easier on BS
+# Hopefully there is no other italicized text that I'm removing
+for i in soup.find_all('i'):
+    i.extract()
+
+# Grab all of the sessionRows from the final set of HTML and work only with that
+sessions = soup.find_all("div", class_="sessionRow")
+
+# Open a blank text file to write sessions to
+file = open("sessions.txt","w")
+# Create a header row for the file. Note the PIPE (|) DELIMITER.
+file.write("Session Number|Session Title|Session Interest|Start Time|End Time|Room and Building\n")
+
+# For each session, pull out the relevant fields and write them to the sessions.txt file.
+for session in sessions:
+    session_soup = BeautifulSoup(str(session), "html5lib")
+    session_id = session_soup.find("div", class_="sessionRow")
+    session_id = session_id['id']
+    session_id = session_id[session_id.find("_")+1:]
+    session_timing = get_session_time(session_id)
+    
+    session_number = session_soup.find("span", class_="abbreviation")
+    session_number = session_number.string.replace(" - ", "")
+
+    session_title = session_soup.find("span", class_="title")
+    session_title = session_title.string.encode('utf-8').rstrip()
+
+    session_abstract = session_soup.find("span", class_="abstract")
+
+    session_interest = session_soup.find("a", class_="interested")
+    
+    if (session_interest == None):
+        session_interest = False
+    else:
+        session_interest = True
+
+    write_contents = str(session_number) + "|" + session_title + "|" + str(session_interest) + "|" + str(session_timing['start_time']) + "|" + str(session_timing['end_time']) + "|" + str(session_timing['room'])
+    file.write(write_contents + "\n")
+    # Print the session title for each session written to the file
+    print (session_title)
+
+file.close()
